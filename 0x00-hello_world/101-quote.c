24a6fe96dca2f029255c37ef6f902fdf62991a88
Your Name <you@example.com>
2022-12-08 17:09:17 -0800

    to print a line of code without printf or puts functions

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print a line of code not using printf or puts
+ * code by TotorBelieve
+ * Return: 1
+*/
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
