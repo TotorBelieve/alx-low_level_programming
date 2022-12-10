f082d0790928446e487ebe7677600db06137b3b1
Your Name <you@example.com>
2022-12-09 16:51:00 -0800

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print alphabet in lower case using putchar
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
