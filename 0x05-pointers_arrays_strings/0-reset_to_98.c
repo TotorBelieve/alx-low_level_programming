b85b8dcb027fe2cd4946db8ce2869651ed27a83c
Your Name <you@example.com>
2022-12-19 03:24:27 -0800

    update int value to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to int n and update value to 98
+ * @n: int to update
+ * code by Wealth_land
+ * Return 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Learning about pointers, Arrays, Strings, and how to use and manipulate them 
